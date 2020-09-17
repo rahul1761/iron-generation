ce95fcaff0f7222305d3649afd654a853c22bae3
pradeep rawat <pradzz.rawat6@gmail.com>
2020-09-17 14:56:20 +0530

    Updates

---
+++ precache-manifest.fe66a99a2473fdafb5c45cb2fd8f7637.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e85c99913dc31307ada2bb53ffca604",
+    "url": "/iron-generation/index.html"
+  },
+  {
+    "revision": "61c329eab64fa3a001a0",
+    "url": "/iron-generation/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6105e47cde297b50a298",
+    "url": "/iron-generation/static/js/2.4901a6b9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/iron-generation/static/js/2.4901a6b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61c329eab64fa3a001a0",
+    "url": "/iron-generation/static/js/main.9e6bfd47.chunk.js"
+  },
+  {
+    "revision": "5d0efe6a79d260445732",
+    "url": "/iron-generation/static/js/runtime-main.195703c0.js"
+  },
+  {
+    "revision": "4a75304279da991579c52e9e4a8a3932",
+    "url": "/iron-generation/static/media/logo.4a753042.jpg"
+  }
+]);
